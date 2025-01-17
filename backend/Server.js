a540b08fc4cad22a04c4922296e4a73070aabfc2
unknown <siddiqaboobakkarsiddiq.com>
2025-01-17 16:10:42 +0530

    second commit

---
+++ backend/Server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
